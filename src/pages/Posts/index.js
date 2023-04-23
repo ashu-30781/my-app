e1d19325bc3d0f6e7310b0b98364fbe216a4508b
Ashutosh <mayank2003mayank@gmail.com>
2023-04-23 14:32:56 +0530

    v1.3

--- src/pages/Posts/index.js
+++ src/pages/Posts/index.js
@@ -1,5 +1,59 @@
+import { useEffect, useState } from "react";
+import Chip from "@mui/material/Chip";
+import Paper from "@mui/material/Paper";
+
+import "./Posts.css";
 function Posts() {
-  return <div>Welcome to Posts </div>;
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    getData(0);
+  }, []);
+
+  function getData(skip) {
+    console.log("skip", skip);
+    fetch(`https://dummyjson.com/posts?skip=${30 * skip}`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("api data", data);
+        setData(data);
+      })
+      .catch((error) => {
+        // handle the error
+      });
+  }
+  const getButton = (total) => {
+    console.log(total);
+    let arr = [];
+    for (let i = 0; i < total; i++) {
+      arr.push(
+        <button onClick={() => getData(i)} key={i + 1}>
+          {i + 1}
+        </button>
+      );
+    }
+    return arr;
+  };
+
+  console.log("sate data", data);
+  return (
+    <div>
+      {data?.total && getButton(data?.total / 30)}
+      {data?.posts.map((value) => (
+        <Paper elevation={3} className="posts-wrap">
+          <h3>
+            {value.id}. {value.title}
+          </h3>
+          <p>{value.body}</p>
+          <ul className="tags-wrap">
+            {value?.tags.map((tag) => (
+              <Chip label={tag} />
+            ))}
+          </ul>
+        </Paper>
+      ))}
+    </div>
+  );
 }
 
 export default Posts;
