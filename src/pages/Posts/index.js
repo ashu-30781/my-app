b44c056b064f131c13ecfebe855db4ac76386b1e
Ashutosh <mayank2003mayank@gmail.com>
2023-04-23 15:23:04 +0530

    v1.4

--- src/pages/Posts/index.js
+++ src/pages/Posts/index.js
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import Chip from "@mui/material/Chip";
 import Paper from "@mui/material/Paper";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import Button from "@mui/material/Button";
 
 import "./Posts.css";
 function Posts() {
@@ -38,20 +40,22 @@ function Posts() {
   console.log("sate data", data);
   return (
     <div>
-      {data?.total && getButton(data?.total / 30)}
-      {data?.posts.map((value) => (
-        <Paper elevation={3} className="posts-wrap">
-          <h3>
-            {value.id}. {value.title}
-          </h3>
-          <p>{value.body}</p>
-          <ul className="tags-wrap">
-            {value?.tags.map((tag) => (
-              <Chip label={tag} />
-            ))}
-          </ul>
-        </Paper>
-      ))}
+      <div className="post-wrap">
+        {data?.posts.map((value) => (
+          <Paper elevation={3} className="posts">
+            <h3>
+              {value.id}. {value.title}
+            </h3>
+            <p>{value.body}</p>
+            <ul className="tags-wrap">
+              {value?.tags.map((tag) => (
+                <Chip label={tag} />
+              ))}
+            </ul>
+          </Paper>
+        ))}
+      </div>
+      <div className="button">{data?.total && getButton(data?.total / 30)}</div>
     </div>
   );
 }
